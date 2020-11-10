5cf737bcde89138eea93c3bb0470e2ef38a8b517
Perrault <lucas.perrault@hotmail.com>
2020-11-10 15:17:12 +0100

    ADD create request

--- weiClinic.js
+++ weiClinic.js
@@ -1,4 +1,3 @@
-import { getNewId } from './idHelper'
 import CorticalStack from './corticalStack'
 import Envelope from './Envelope'
 
@@ -8,13 +7,10 @@ class WeiClinic {
         this.stacks = []
     }
 
-    create(realGender, name, age) {
-        const stackId = getNewId(this.stacks)
-        const envelopeId = getNewId(this.envelopes)
+    create(stackId, envelopeId, realGender, name, age) {
         const newStack = new CorticalStack(stackId, realGender, name, age, envelopeId)
-        const newEnvelope = new Envelope(envelopeId, realGender, age, stackId)
+        const newEnvelope = new Envelope(envelopeId, realGender, age, stackId);
 
-        // It's better like this.. method push is deprecated.
         this.stacks = [
             ...this.stacks,
             newStack
