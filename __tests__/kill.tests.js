a04344db462f08f3cc24f69d298789aaae24ebf0
Perrault <lucas.perrault@hotmail.com>
2020-11-14 11:16:01 +0100

    MOD KILL

--- __tests__/kill.tests.js
+++ __tests__/kill.tests.js
@@ -7,17 +7,22 @@ const ENVELOPE_ID = 1
 const FAKE_ENVELOPE_ID = 88
 const STACK_ID = 1
 
+const mockFindEnvelope = jest.fn();
+const mockKillEnvelope = jest.fn();
+const fakeEnvelope = {
+    id: ENVELOPE_ID,
+    idStack: STACK_ID
+}
+
 beforeEach(() => {
     clinicDependency.getClinic = jest.fn().mockReturnValue({
-        envelopes: [ {
-            id: ENVELOPE_ID,
-            idStack: STACK_ID
-        }],
+        envelopes: [fakeEnvelope],
         stacks: [ {
             id: STACK_ID,
             idEnvelope: ENVELOPE_ID
         }],
-        killEnvelope: jest.fn(),
+        killEnvelope: mockKillEnvelope,
+        findEnvelope: mockFindEnvelope
     })
 })
 
@@ -25,35 +30,38 @@ describe('Kill action', () => {
     it('When data is fine', (done) => {
         const expectedResponse = 204
 
+        mockFindEnvelope.mockReturnValue(fakeEnvelope)
         const killEnvelope = jest.fn().mockReturnValue(expectedResponse)
         clinicDependency.getClinic.mockReturnValue({
-            killEnvelope
+            killEnvelope,
+            findEnvelope: mockFindEnvelope
         })
 
 
         request(app)
             .post(`/kill/${ENVELOPE_ID}`)
-            .expect(204)
+            .expect(expectedResponse)
             .expect(() => {
                 expect(killEnvelope).toHaveBeenCalledTimes(1)
-                expect(killEnvelope).toHaveBeenCalledWith(ENVELOPE_ID)
+                expect(killEnvelope).toHaveBeenCalledWith(fakeEnvelope)
             })
             .end(done)
     });
     it('When data is not fine', (done) => {
         const expectedResponse = 400
+        mockFindEnvelope.mockReturnValue(null)
 
         const killEnvelope = jest.fn().mockReturnValue(expectedResponse)
         clinicDependency.getClinic.mockReturnValue({
-            killEnvelope
+            killEnvelope,
+            findEnvelope: mockFindEnvelope
         })
 
         request(app)
             .post(`/kill/${FAKE_ENVELOPE_ID}`)
-            .expect(400)
+            .expect(expectedResponse)
             .expect(() => {
-                expect(killEnvelope).toHaveBeenCalledTimes(1)
-                expect(killEnvelope).toHaveBeenCalledWith(FAKE_ENVELOPE_ID)
+                expect(killEnvelope).not.toHaveBeenCalled()
             })
             .end(done)
     })
