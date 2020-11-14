7cbe15897c26c02c529c78e1c7070d861c6d2207
Perrault <lucas.perrault@hotmail.com>
2020-11-14 12:27:08 +0100

    FIX kill

--- __tests__/kill.tests.js
+++ __tests__/kill.tests.js
@@ -4,77 +4,57 @@ import app from '../app'
 import * as clinicDependency from '../weiClinic'
 import * as weiClinicServiceDependency from "../weiClinicService";
 
-const ENVELOPE_ID = 1
+const ENVELOPE_ID = 3
 const FAKE_ENVELOPE_ID = 88
-const STACK_ID = 1
+const STACK_ID = 3
 
-const mockKillEnvelope = jest.fn();
+let mockKillEnvelope = jest.fn();
 const mockFindEnvelope = jest.fn();
 const fakeEnvelope = {
     id: ENVELOPE_ID,
     idStack: STACK_ID
 }
 
+jest.mock('../dal.js', () => {
+    return jest.fn().mockImplementation(() => ({
+        getAllStacksAsync: jest.fn().mockReturnValue(new Promise(() => [])),
+        getAllEnvelopesAsync: jest.fn().mockReturnValue(new Promise(() =>[])),
+        removeEnvelopeByIdAsync: jest.fn(),
+        updateEnvelopeIdFromStackAsync: jest.fn()
+    }))
+})
+
 beforeEach(() => {
-    weiClinicServiceDependency.getClinicService = jest.fn().mockReturnValue({
-        dal: jest.fn().mockImplementation()
-    }),
     clinicDependency.getClinic = jest.fn().mockReturnValue({
         envelopes: [fakeEnvelope],
         stacks: [ {
             id: STACK_ID,
             idEnvelope: ENVELOPE_ID
         }],
-        killEnvelope: mockKillEnvelope,
-        findEnvelope: mockFindEnvelope
+        findEnvelope: mockFindEnvelope,
+        killEnvelope: mockKillEnvelope
     })
 })
 
 describe('Kill action', () => {
-    it('When data is fine', (done) => {
+    it('When data is fine', async () => {
         const expectedResponse = 204
-
         mockFindEnvelope.mockReturnValue(fakeEnvelope)
 
-        request(app)
-            .post(`/kill/${ENVELOPE_ID}`)
-            .expect(expectedResponse)
-            .expect(() => {
-                expect(mockKillEnvelope).toHaveBeenCalledTimes(1)
-                expect(mockKillEnvelope).toHaveBeenCalledWith(fakeEnvelope)
-            })
-            .end(done)
-    });
-    it('When data is fine', (done) => {
-        const expectedResponse = 204
+        const response = await request(app).post(`/kill/${ENVELOPE_ID}`)
 
-        mockFindEnvelope.mockReturnValue(fakeEnvelope)
-
-        request(app)
-            .post(`/kill/${ENVELOPE_ID}`)
-            .expect(expectedResponse)
-            .expect(() => {
-                expect(mockKillEnvelope).toHaveBeenCalledTimes(1)
-                expect(mockKillEnvelope).toHaveBeenCalledWith(fakeEnvelope)
-            })
-            .end(done)
+        expect(response.status).toBe(expectedResponse)
+        expect(mockKillEnvelope).toHaveBeenCalledTimes(1)
+        expect(mockKillEnvelope).toHaveBeenCalledWith(fakeEnvelope)
     });
-    it('When data is not fine', (done) => {
+    it('When data is not fine', async () => {
         const expectedResponse = 400
         mockFindEnvelope.mockReturnValue(null)
+        mockKillEnvelope = jest.fn()
 
-        const killEnvelope = jest.fn().mockReturnValue(expectedResponse)
-        clinicDependency.getClinic.mockReturnValue({
-            killEnvelope,
-            findEnvelope: mockFindEnvelope
-        })
+        const res = await request(app).post(`/kill/${FAKE_ENVELOPE_ID}`)
 
-        request(app)
-            .post(`/kill/${FAKE_ENVELOPE_ID}`)
-            .expect(expectedResponse)
-            .expect(() => {
-                expect(killEnvelope).not.toHaveBeenCalled()
-            })
-            .end(done)
+        expect(res.status).toBe(expectedResponse)
+        expect(mockKillEnvelope).not.toHaveBeenCalled()
     })
 })
