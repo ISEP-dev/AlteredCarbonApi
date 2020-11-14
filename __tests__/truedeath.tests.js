17208d87fa603c18be10ae5c795ba19ed5c0903b
Perrault <lucas.perrault@hotmail.com>
2020-11-14 11:16:12 +0100

    MOD TRUEDEATH

--- __tests__/truedeath.tests.js
+++ __tests__/truedeath.tests.js
@@ -2,34 +2,36 @@ import * as clinicDependency from "../weiClinic";
 import request from "supertest";
 import app from "../app";
 
-
+const mockFindStack = jest.fn()
 const EXPECTED_ENVELOPE_ID = 1;
+
 const EXPECTED_STACK_ID = 1;
 const EXPECTED_FAKE_STACK_ID = 99;
+const fakeStack = {
+    id: EXPECTED_STACK_ID,
+    idEnvelope: EXPECTED_ENVELOPE_ID
+}
 
 beforeEach(() => {
     clinicDependency.getClinic = jest.fn().mockReturnValue({
-        stacks: [
-            {
-                id: EXPECTED_STACK_ID,
-                idEnvelope: EXPECTED_ENVELOPE_ID
-            }
-        ],
+        stacks: [fakeStack],
         envelopes: [
             {
                 id: EXPECTED_ENVELOPE_ID,
                 idStack: EXPECTED_STACK_ID
             }
         ],
-        destroyStack: jest.fn()
+        destroyStack: jest.fn(),
+        findStack: mockFindStack
     })
 })
 
 
 describe('True Death action', () => {
     it('Stack exist return No Content', (done) => {
-
         const queryStackId = EXPECTED_STACK_ID;
+
+        mockFindStack.mockReturnValue(fakeStack)
         const destroyStack = jest.spyOn(clinicDependency.getClinic(), 'destroyStack').mockReturnValue(204)
 
         request(app)
@@ -37,23 +39,22 @@ describe('True Death action', () => {
             .expect(204)
             .expect(() => {
                 expect(destroyStack).toHaveBeenCalledTimes(1)
-                expect(destroyStack).toHaveBeenCalledWith(EXPECTED_STACK_ID)
+                expect(destroyStack).toHaveBeenCalledWith(fakeStack)
                 expect(destroyStack).toHaveReturnedWith(204)
             }).end(done)
     })
 
     it("Stack doesn't exists return Bad Request ", (done) => {
-
         const queryStackId = EXPECTED_STACK_ID;
+
+        mockFindStack.mockReturnValue(null)
         const destroyStack = jest.spyOn(clinicDependency.getClinic(), 'destroyStack').mockReturnValue(400)
 
         request(app)
             .delete(`/truedeath/${queryStackId}`)
             .expect(400)
             .expect(() => {
-                expect(destroyStack).toHaveBeenCalledTimes(1)
-                expect(destroyStack).toHaveBeenCalledWith(EXPECTED_STACK_ID)
-                expect(destroyStack).toHaveReturnedWith(400)
+                expect(destroyStack).not.toHaveBeenCalled()
             }).end(done)
     })
 })
