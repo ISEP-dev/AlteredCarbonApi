79aa29b2d897a56990f3ae0f7980839446af95e6
Perrault <lucas.perrault@hotmail.com>
2020-11-14 12:27:21 +0100

    FIX truedeath

--- __tests__/truedeath.tests.js
+++ __tests__/truedeath.tests.js
@@ -3,14 +3,22 @@ import request from "supertest";
 import app from "../app";
 
 const mockFindStack = jest.fn()
-const EXPECTED_ENVELOPE_ID = 2;
+const EXPECTED_ENVELOPE_ID = 22;
 
-const EXPECTED_STACK_ID = 2;
-const EXPECTED_FAKE_STACK_ID = 99;
+const EXPECTED_STACK_ID = 25;
 const fakeStack = {
     id: EXPECTED_STACK_ID,
     idEnvelope: EXPECTED_ENVELOPE_ID
 }
+
+jest.mock('../dal.js', () => {
+    return jest.fn().mockImplementation(() => ({
+        getAllStacksAsync: jest.fn().mockReturnValue(new Promise(() => [])),
+        getAllEnvelopesAsync: jest.fn().mockReturnValue(new Promise(() =>[])),
+        removeStackByIdAsync: jest.fn(),
+        removeEnvelopeByIdAsync: jest.fn()
+    }))
+})
 
 beforeEach(() => {
     clinicDependency.getClinic = jest.fn().mockReturnValue({
@@ -28,33 +36,26 @@ beforeEach(() => {
 
 
 describe('True Death action', () => {
-    it('Stack exist return No Content', (done) => {
+    it('Stack exist return No Content', async () => {
         const queryStackId = EXPECTED_STACK_ID;
 
         mockFindStack.mockReturnValue(fakeStack)
         const destroyStack = jest.spyOn(clinicDependency.getClinic(), 'destroyStack').mockReturnValue(204)
 
-        request(app)
-            .delete(`/truedeath/${queryStackId}`)
-            .expect(204)
-            .expect(() => {
-                expect(destroyStack).toHaveBeenCalledTimes(1)
-                expect(destroyStack).toHaveBeenCalledWith(fakeStack)
-                expect(destroyStack).toHaveReturnedWith(204)
-            }).end(done)
+        const res = await request(app).delete(`/truedeath/${queryStackId}`)
+        expect(res.status).toBe(204)
+        expect(destroyStack).toHaveBeenCalledTimes(1)
+        expect(destroyStack).toHaveBeenCalledWith(fakeStack)
     })
 
-    it("Stack doesn't exists return Bad Request ", (done) => {
+    it("Stack doesn't exists return Bad Request ", async () => {
         const queryStackId = EXPECTED_STACK_ID;
 
         mockFindStack.mockReturnValue(null)
         const destroyStack = jest.spyOn(clinicDependency.getClinic(), 'destroyStack').mockReturnValue(400)
 
-        request(app)
-            .delete(`/truedeath/${queryStackId}`)
-            .expect(400)
-            .expect(() => {
-                expect(destroyStack).not.toHaveBeenCalled()
-            }).end(done)
+        const res = await request(app).delete(`/truedeath/${queryStackId}`)
+        expect(res.status).toBe(400)
+        expect(destroyStack).not.toHaveBeenCalled()
     })
 })
