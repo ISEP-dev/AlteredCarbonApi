c8f67f82e32c04b0b128addbe665f067df469356
Perrault <lucas.perrault@hotmail.com>
2020-11-14 11:15:56 +0100

    MOD IMPLANT

--- __tests__/implant.tests.js
+++ __tests__/implant.tests.js
@@ -3,32 +3,41 @@ import app from '../app'
 
 import * as clinicDependency from '../weiClinic'
 
+const mockFindStack = jest.fn();
+const mockFindEnvelope = jest.fn();
+
 const ENVELOPE_ID = 1
-const FAKE_ENVELOPE_ID = 88
 const AVAILABLE_ENVELOPE_ID = 2
 const STACK_ID = 1
-const OTHER_STACK_ID = 2
 const FAKE_STACK_ID = 88
+
+const fakeStack = {
+    id: STACK_ID,
+    idEnvelope: ENVELOPE_ID
+}
+const fakeEnvelope = {
+    id: ENVELOPE_ID,
+    idStack: STACK_ID
+}
+const fakeAvailableEnvelope = {
+    id: AVAILABLE_ENVELOPE_ID,
+    idStack: null
+}
 
 beforeEach(() => {
     clinicDependency.getClinic = jest.fn().mockReturnValue({
-        envelopes: [
-            {
-                id: ENVELOPE_ID,
-                idStack: STACK_ID
-            }
-        ],
-        stacks: [ {
-            id: STACK_ID,
-            idEnvelope: ENVELOPE_ID
-        }],
+        envelopes: [fakeEnvelope],
+        stacks: [fakeStack],
         assignStackToEnvelope: jest.fn(),
+        findStack: mockFindStack,
+        findEnvelope: mockFindEnvelope
     })
 })
 
 describe('Implant action', () => {
-
     it('When stackId is not fine', (done) => {
+
+        mockFindStack.mockReturnValue(null)
         const assignStackToEnvelope = jest.spyOn(clinicDependency.getClinic(), 'assignStackToEnvelope');
 
         request(app)
@@ -40,6 +49,9 @@ describe('Implant action', () => {
             .end(done)
     });
     it('When stackId & envelopId are fine', (done) => {
+
+        mockFindStack.mockReturnValue(fakeStack)
+        mockFindEnvelope.mockReturnValue(fakeEnvelope)
         const assignStackToEnvelope = jest.spyOn(clinicDependency.getClinic(), 'assignStackToEnvelope');
 
         request(app)
@@ -47,7 +59,7 @@ describe('Implant action', () => {
             .expect(204)
             .expect(() => {
                 expect(assignStackToEnvelope).toHaveBeenCalledTimes(1)
-                expect(assignStackToEnvelope).toHaveBeenCalledWith(STACK_ID, ENVELOPE_ID)
+                expect(assignStackToEnvelope).toHaveBeenCalledWith(fakeStack, ENVELOPE_ID)
             })
             .end(done)
     });
@@ -65,9 +77,13 @@ describe('Implant action', () => {
     it('When you only have stackId and available envelopeId', (done) => {
         const assignStackToEnvelope = jest.spyOn(clinicDependency.getClinic(), 'assignStackToEnvelope');
 
+        mockFindStack.mockReturnValue(fakeStack)
+        mockFindEnvelope.mockReturnValue(fakeAvailableEnvelope)
 
         clinicDependency.getClinic = jest.fn().mockReturnValue({
             ...clinicDependency.getClinic(),
+            findStack: mockFindStack,
+            findEnvelope: mockFindEnvelope,
             envelopes: [
                 {
                     id: AVAILABLE_ENVELOPE_ID,
@@ -81,7 +97,7 @@ describe('Implant action', () => {
             .expect(204)
             .expect(() => {
                 expect(assignStackToEnvelope).toHaveBeenCalledTimes(1)
-                expect(assignStackToEnvelope).toHaveBeenCalledWith(STACK_ID, AVAILABLE_ENVELOPE_ID)
+                expect(assignStackToEnvelope).toHaveBeenCalledWith(fakeStack, AVAILABLE_ENVELOPE_ID)
             })
             .end(done)
     });
