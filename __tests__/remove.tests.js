0e1c4f3c6668dbcfbc00e3208d34621853fe7f32
Perrault <lucas.perrault@hotmail.com>
2020-11-14 12:27:13 +0100

    FIX remove

--- __tests__/remove.tests.js
+++ __tests__/remove.tests.js
@@ -2,8 +2,8 @@ import request from 'supertest'
 import app from '../app'
 import * as clinicDependency from "../weiClinic";
 
-const EXPECTED_ENVELOPE_ID = 1;
-const EXPECTED_STACK_ID = 1;
+const EXPECTED_ENVELOPE_ID = 22;
+const EXPECTED_STACK_ID = 25;
 const EXPECTED_FAKE_STACK_ID = 99;
 
 const mockFindStack = jest.fn()
@@ -11,6 +11,15 @@ const fakeStack = {
     id: EXPECTED_STACK_ID,
     idEnvelope: EXPECTED_ENVELOPE_ID
 }
+
+jest.mock('../dal.js', () => {
+    return jest.fn().mockImplementation(() => ({
+        getAllStacksAsync: jest.fn().mockReturnValue(new Promise(() => [])),
+        getAllEnvelopesAsync: jest.fn().mockReturnValue(new Promise(() =>[])),
+        updateStackIdFromEnvelopeAsync: jest.fn(),
+        updateEnvelopeIdFromStackAsync: jest.fn()
+    }))
+})
 
 beforeEach(() => {
     clinicDependency.getClinic = jest.fn().mockReturnValue({
@@ -23,33 +32,27 @@ beforeEach(() => {
 })
 
 describe('Remove action', () => {
-    it('Removing a stack is ok', (done) => {
+    it('Removing a stack is ok', async () => {
         const queryStackId = EXPECTED_STACK_ID;
 
         mockFindStack.mockReturnValue(fakeStack)
         const removeStackFromEnvelope = jest.spyOn(clinicDependency.getClinic(), 'removeStackFromEnvelope')
 
-        request(app)
-            .post(`/remove/${queryStackId}`)
-            .expect(204)
-            .expect(() => {
-                expect(removeStackFromEnvelope).toHaveBeenCalledTimes(1)
-                expect(removeStackFromEnvelope).toHaveBeenCalledWith(fakeStack)
-            }).end(done)
+        const res = await request(app).post(`/remove/${queryStackId}`)
+        expect(res.status).toBe(204)
+        expect(removeStackFromEnvelope).toHaveBeenCalledTimes(1)
+        expect(removeStackFromEnvelope).toHaveBeenCalledWith(fakeStack)
     })
 
-    it('Removing a stack failed', (done) => {
+    it('Removing a stack failed', async () => {
         const queryStackId = EXPECTED_FAKE_STACK_ID;
 
         mockFindStack.mockReturnValue(null)
 
         const removeStackFromEnvelope = jest.spyOn(clinicDependency.getClinic(), 'removeStackFromEnvelope')
 
-        request(app)
-            .post(`/remove/${queryStackId}`)
-            .expect(400)
-            .expect(() => {
-                expect(removeStackFromEnvelope).not.toHaveBeenCalled()
-            }).end(done)
+        const res = await request(app).post(`/remove/${queryStackId}`)
+        expect(res.status).toBe(400)
+        expect(removeStackFromEnvelope).not.toHaveBeenCalled()
     })
 })
